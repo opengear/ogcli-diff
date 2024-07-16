b572aebd6346a05c52eeda68a86aa518883c215f
Pai Matt <20845116+mattwit@users.noreply.github.com>
2024-07-16 16:49:44 -0400

    Embedded fields now show '-'

--- ogcli-diff.py
+++ ogcli-diff.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
-#
-# ogcli-diff.py
-
 import argparse
+import difflib
 
 class ConfigEntry:
     def __init__(self):
         self.ogcliCmd = ""
         self.comments = []
         self.section = []
-        self.lc = "+"
         
     def has_content(self):
         return len(self.section) > 0
@@ -23,16 +19,13 @@ class ConfigEntry:
     def __str__(self):
         result = ""
         for comment in self.comments:
-            result += self.lc + comment + "\n"
-
-        result += self.lc+ self.ogcliCmd + "\n"
-
+            result += comment + "\n"
+        result += self.ogcliCmd + "\n"
         for entry in self.section:
             if "END" in entry:
-                result += self.lc + entry + " " + "\n"
+                result += entry + " " + "\n"
             else:
-                result += self.lc + "  " + entry + " " + "\n"
-
+                result += "  " + entry + " " + "\n"
         result += "\n\n"
         return result
 
@@ -48,23 +41,14 @@ class ConfigEntry:
     def set_comment(self, comment):
         self.comments.append(comment)
 
-    def set_lead_char(self, lc):
-        self.lc = lc
 
 def parse_config_line_array(lines_from_file):
     sections = []
-    version = ""
-    sku = ""
     in_section = False
     section = ConfigEntry()
 
     for line in lines_from_file:
-        if "VERSION" in line:
-            version = line
-        if "SKU" in line:
-            sku = line
-
-        if line and line[0] == '#':
+        if line.startswith('#'):
             if not in_section:
                 section.set_comment(line)
             else:
@@ -97,14 +81,6 @@ def parse_config_line_array(lines_from_file):
                 sections.append(section)
             section = ConfigEntry()
 
-        if line and line[0] == '#' and lines_from_file.index(line) + 1 != len(lines_from_file):
-            next_line = lines_from_file[lines_from_file.index(line) + 1]
-            if next_line and next_line[0] == '#':
-                in_section = False
-                if section.has_content():
-                    sections.append(section)
-                section = ConfigEntry()
-
     if section.has_content():
         sections.append(section)
 
@@ -119,46 +95,72 @@ def read_lines_from_file(filename):
     return lines_from_file
 
 
-def generate_diff(map1, map2):
-    for key, value in map1.items():
-        if key in map2 and value != map2[key]:
-            value.set_lead_char("+")
-            section1 = map1[key]
+def generate_diff(map1, map2, file1_name, file2_name):
+    diffs = []
+
+    for key, section1 in map1.items():
+        if key in map2:
             section2 = map2[key]
             if section1 != section2:
-                print(section1)
-                print(f"")
+                section1_lines = [section1.ogcliCmd] + section1.comments + section1.section
+                section2_lines = [section2.ogcliCmd] + section2.comments + section2.section
+                diff = difflib.unified_diff(
+                    section2_lines,
+                    section1_lines,
+                    fromfile=file2_name,
+                    tofile=file1_name,
+                    lineterm=''
+                )
+                diffs.extend(diff)
 
-            value.set_lead_char("")
-            
     for key in map2:
         if key not in map1:
-#            print(f"Key '{key}' is present in dictionary 2 but not in dictionary 1.\n")
-            value = map2[key]
-            value.set_lead_char("-")
-            print(value)
-            value.set_lead_char("")
+            section2 = map2[key]
+            section2_lines = [section2.ogcliCmd] + section2.comments + section2.section
+            diff = difflib.unified_diff(
+                [],
+                section2_lines,
+                fromfile=file2_name,
+                tofile=file1_name,
+                lineterm=''
+            )
+            diffs.extend(diff)
 
     for key in map1:
         if key not in map2:
-            value = map1[key]
-#           print(f"Key '{key}' is present in dictionary 1 but not in dictionary 2.\n")
-            value.set_lead_char("+")
-            print(value)
-            value.set_lead_char("")
+            section1 = map1[key]
+            section1_lines = [section1.ogcliCmd] + section1.comments + section1.section
+            diff = difflib.unified_diff(
+                section1_lines,
+                [],
+                fromfile=file2_name,
+                tofile=file1_name,
+                lineterm=''
+            )
+            diffs.extend(diff)
+
+    filtered_diffs = []
+    skip = False
+    for line in diffs:
+        if line.startswith('@@'):
+            skip = True
+            continue
+        if skip and (line.startswith('-') or line.startswith('+') or line.startswith(' ')):
+            filtered_diffs.append(line)
+        else:
+            skip = False
+
+    print('\n'.join(filtered_diffs))
 
 
-            
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Compare two ogcli configuration files.')
 
     parser.add_argument('config1', metavar='config1.txt', type=str, help='path to the first configuration file')
-
     parser.add_argument('config2', metavar='config2.txt', type=str, help='path to the second configuration file')
 
     args = parser.parse_args()
-    
+
     lines_from_file1 = read_lines_from_file(args.config1)
     lines_from_file2 = read_lines_from_file(args.config2)
 
@@ -169,4 +171,4 @@ if __name__ == "__main__":
     file2_dictionary = {section.ogcliCmd: section for section in file2_sections}
 
     # Generate diff
-    generate_diff(file1_dictionary, file2_dictionary)
+    generate_diff(file1_dictionary, file2_dictionary, args.config1, args.config2)
